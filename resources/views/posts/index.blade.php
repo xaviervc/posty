f3766f8ebb10a4f30b25fb6a503662ee35f23e71
Xavier Vani-Charron <xavier.vani@gmail.com>
2021-01-18 23:03:07 -0500

    initial commit

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="flex justify-center">
+        <div class="w-8/12 bg-white p-6 rounded-lg">
+            <form action="{{ route('posts') }}" method="post">
+                @csrf
+                <div class="mb-4">
+                    <label for="body" class="sr-only">Body</label>
+                    <textarea name="body" id="body" placeholder="Post something!" class="bg-gray-100 border-2 w-full p-4 rounded-lg @error('body') border-red-500 @enderror"></textarea>
+
+                    @error('body')
+                        <div class="text-red-500 mt-2 text-sm">
+                            {{ $message }}
+                        </div>
+                    @enderror
+                </div>
+                <div class="mb-4">
+                    <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded font-medium">Post</button>
+                </div>
+            </form>
+
+            @if ($posts->count())
+                @foreach ($posts as $post)
+                    <div class="mb-4">
+                        <a href="{{ route('users.post', $post->user)}}" class="font-bold">{{ $post->user->name }}</a> <span class="text-gray-600 text-sm">{{$post->created_at->diffForHumans()}}</span>
+                        <p class="mb-2">{{ $post->body }}</p>
+
+                        @can('delete', $post)
+                        <form action="{{ route('posts.destroy', $post) }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="text-blue-500">Delete</button>
+                        </form>
+                        @endcan
+
+                        <div class="flex items-center">
+                            @auth
+                            @if (!$post->likedBy(auth()->user()))
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    <button type="submit" class="text-blue-500">Like</button>
+                                </form>
+                            @else
+                                <form action="{{ route('posts.likes', $post) }}" method="post" class="mr-1">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="text-blue-500">Unlike</button>
+                                </form>
+                            @endif
+                            @endauth
+                            <span>{{$post->likes->count() }} {{ Str::plural('like', $post->likes->count()) }}</span>
+                        </div>
+                    </div>
+                @endforeach
+                
+                {{ $posts->links() }}
+            @else
+                <p>There are no posts</p>
+            @endif
+        </div>
+    </div>
+@endsection
